c012400092c645847d0bd86a9312655e6827df79
Nicholas Konar <nkonar@protonmail.com>
2025-01-13 04:36:26 -0500

    display results

--- monty-hall.js
+++ monty-hall.js
@@ -1,9 +1,26 @@
+const random = (min, max) => {
+    const range = (max - min + 1)
+    const n = Math.random() * range
+    return Math.floor(n) + min
+}
+
 document.addEventListener("DOMContentLoaded", () => {
 
-    const random = (min, max) => {
-        const range = (max - min + 1)
-        const n = Math.random() * range
-        return Math.floor(n) + min
+    var simCount = 0
+    var winCount = 0
+
+    const clearScore = () => {
+        simCount = 0
+        winCount = 0
+    }
+
+    const updateScore = (outcome) => {
+        simCount++
+        winCount += outcome
+        const rawPercent = (winCount / simCount) * 100
+        const percent = parseFloat(rawPercent.toFixed(2))
+        const results = document.getElementById('results')
+        results.textContent = `you won ${winCount}/${simCount} or ${percent}%`
     }
 
     const runOnce = () => {
@@ -13,17 +30,18 @@ document.addEventListener("DOMContentLoaded", () => {
         const quantity = parseInt(doorsInput.value, 10) - 1
         const winner = random(0, quantity)
         const choice = random(0, quantity)
-        return change.checked ? winner != choice : winner == choice
+        const outcome = change.checked ? winner != choice : winner == choice
+        updateScore(outcome)
     }
 
     const run = () => {
         const rounds = document.getElementById("rounds")
         const n = parseInt(rounds.value, 10)
-        const results = Array.from({ length: n })
+        Array.from({ length: n })
             .map(e => runOnce())
             .filter(i => i)
-        console.log(results.length, n, results.length / n)
     }
 
+    document.getElementById("run").addEventListener("click", clearScore)
     document.getElementById("run").addEventListener("click", run)
 })
