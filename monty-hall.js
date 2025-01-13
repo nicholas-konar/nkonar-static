c6a2387b13bc2c07b544c9e59e1bc20eead55559
Nicholas Konar <nkonar@protonmail.com>
2025-01-13 04:08:33 -0500

    run n simulations

--- monty-hall.js
+++ monty-hall.js
@@ -1,20 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-    const makeDoors = () => {
-        const input = document.getElementById("doors")
-        const quantity = parseInt(input.value, 10)
-        const doors = new Array(quantity).fill(false)
-        const winner = Math.floor(Math.random() * quantity)
-        doors[winner] = true
-        return doors
+    const random = (min, max) => {
+        const range = (max - min + 1)
+        const n = Math.random() * range
+        return Math.floor(n) + min
+    }
+
+    const runOnce = () => {
+        // did you choose correctly the first time?
+        const change = document.getElementById("change")
+        const doorsInput = document.getElementById("doors")
+        const quantity = parseInt(doorsInput.value, 10) - 1
+        const winner = random(0, quantity)
+        const choice = random(0, quantity)
+        return change.checked ? winner != choice : winner == choice
     }
 
     const run = () => {
         const rounds = document.getElementById("rounds")
-        const change = document.getElementById("change")
-        const doors = makeDoors()
-
-        console.log(rounds.value, change.checked, doors)
+        const n = parseInt(rounds.value, 10)
+        const results = Array.from({ length: n })
+            .map(e => runOnce())
+            .filter(i => i)
+        console.log(results.length, n, results.length / n)
     }
 
     document.getElementById("run").addEventListener("click", run)
